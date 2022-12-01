3ae4e840ac3ec94bf587fd26b84e4df46a6d07c2
MatteoGisondi <matteo.gisondi@gmail.com>
2022-12-01 01:30:10 -0500

    feat:

---
+++ 2022/01/a.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from itertools import groupby
+
+path = Path(__file__)
+
+f_in = f'{path.parent}/{path.stem}.in'
+
+with open(f_in, 'r') as f:
+    lines = f.read()
+
+res = [list(map(int, sub)) for _, sub in groupby(lines.split('\n'), key=bool) if _]
+print(res)
+answer = max(map(sum, res))
+
+print(answer)
