3f2bc01178f8b4b9231f23668b11324a0534a274
MatteoGisondi <matteo.gisondi@gmail.com>
2022-12-07 07:56:55 -0500

    Solved!

--- problems/2022/07/a.py
+++ problems/2022/07/a.py
@@ -5,11 +5,51 @@ path = Path(__file__)
 f_in = f'{path.parent}/.in'
 
 with open(f_in, 'r') as f:
-    lines = list(map(int, f.readlines()))
+    lines = list(map(lambda x: x.split(), f.readlines()))
 
 answer = 0
 
-for line in lines:
-    pass
+class ParentDict(dict):
+    def __init__(self, parent):
+        self.parent = parent
+        self.size = 0
+
+    def add(self, size):
+        self.size += size
+        parent = self.parent
+        while parent is not None:
+            parent.size += size
+            parent = parent.parent
+
+file_structure = {}
+cwd = file_structure["/"] = ParentDict(None)
+
+for line in lines[1:]:
+    match line:
+        case ["$", "cd", directory]:
+            if directory == "..":
+                cwd = cwd.parent
+            else:
+                cwd = ParentDict(cwd)
+                cwd.parent[directory] = cwd
+        case ["$", "ls"]:
+            pass
+        case ["dir", directory]:
+            cwd[directory] = ParentDict(cwd)
+        case [size, file]:
+            cwd[file] = int(size)
+            cwd.add(int(size))
+
+answer = 0
+
+def get_size(directory):
+    global answer
+    for child in directory.values():
+        if isinstance(child, ParentDict):
+            if child.size < 100000:
+                answer += child.size
+            get_size(child)
+
+get_size(file_structure)
 
 print(answer)
--- problems/2022/07/b.py
+++ problems/2022/07/b.py
@@ -5,11 +5,55 @@ path = Path(__file__)
 f_in = f'{path.parent}/.in'
 
 with open(f_in, 'r') as f:
-    lines = list(map(int, f.readlines()))
+    lines = list(map(lambda x: x.split(), f.readlines()))
 
 answer = 0
 
-for line in lines:
-    pass
+class ParentDict(dict):
+    def __init__(self, parent):
+        self.parent = parent
+        self.size = 0
+
+    def add(self, size):
+        self.size += size
+        parent = self.parent
+        while parent is not None:
+            parent.size += size
+            parent = parent.parent
+
+file_structure = {}
+cwd = file_structure["/"] = ParentDict(None)
+total_size = 0
+
+for i, line in enumerate(lines[1:], start=2):
+    match line:
+        case ["$", "cd", directory]:
+            if directory == "..":
+                cwd = cwd.parent
+            else:
+                cwd = ParentDict(cwd)
+                cwd.parent[directory] = cwd
+        case ["$", "ls"]:
+            pass
+        case ["dir", directory]:
+            cwd[directory] = ParentDict(cwd)
+        case [size, file]:
+            cwd[file] = int(size)
+            cwd.add(int(size))
+
+answer = file_structure["/"].size
+
+min_space = answer - 70000000 + 30000000
+
+def candidate(directory):
+    global answer
+    for child in directory.values():
+        if isinstance(child, ParentDict):
+            if child.size >= min_space:
+                if child.size < answer:
+                    answer = child.size
+                candidate(child)
+
+candidate(file_structure)
 
 print(answer)
