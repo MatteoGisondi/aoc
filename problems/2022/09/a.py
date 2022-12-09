ebcf22d88557aa12d76879ef985ba01f430f27f2
MatteoGisondi <matteo.gisondi@gmail.com>
2022-12-09 15:09:24 -0500

    Solved!

--- problems/2022/09/a.py
+++ problems/2022/09/a.py
@@ -1,15 +1,44 @@
 from pathlib import Path
+import itertools
 
 path = Path(__file__)
 
 f_in = f'{path.parent}/.in'
 
+DIR = {"U": (0, 1), "L": (-1, 0), "D": (0, -1), "R": (1, 0)}
+
 with open(f_in, 'r') as f:
-    lines = list(map(int, f.readlines()))
+    lines = list(map(lambda x: x.split(), f.readlines()))
 
-answer = 0
+x_H, y_H = 0, 0
+x_T, y_T = 0, 0
 
-for line in lines:
-    pass
+visited = set()
 
-print(answer)
+for direction, distance in lines:
+    movement = DIR[direction]
+    for _ in range(int(distance)):
+        x_H += movement[0]
+        y_H += movement[1]
+
+        if x_T - x_H == 2:
+            if y_T != y_H:
+                y_T = y_H
+            x_T -= 1
+        elif x_H - x_T == 2:
+            if y_T != y_H:
+                y_T = y_H
+            x_T += 1
+
+        if y_T - y_H == 2:
+            if x_T != x_H:
+                x_T = x_H
+            y_T -= 1
+        elif y_H - y_T == 2:
+            if x_T != x_H:
+                x_T = x_H
+            y_T += 1
+
+        visited.add((x_T, y_T))
+
+print(len(visited))
--- problems/2022/09/b.py
+++ problems/2022/09/b.py
@@ -4,12 +4,51 @@ path = Path(__file__)
 
 f_in = f'{path.parent}/.in'
 
+DIR = {"U": (0, 1), "L": (-1, 0), "D": (0, -1), "R": (1, 0)}
+
 with open(f_in, 'r') as f:
-    lines = list(map(int, f.readlines()))
+    lines = list(map(lambda x: x.split(), f.readlines()))
 
-answer = 0
+knots = [(0, 0)] * 10
 
-for line in lines:
-    pass
+visited = set()
 
-print(answer)
+for direction, distance in lines:
+    movement = DIR[direction]
+    for _ in range(int(distance)):
+        knots[0] = (knots[0][0] + movement[0], knots[0][1] + movement[1])
+
+        for i in range(1, len(knots)):
+            x_H, y_H = knots[i - 1]
+            x_T, y_T = knots[i]
+
+            if abs(x_H - x_T) <= 1 and abs(y_H - y_T) <= 1:
+                # no movement necessary
+                pass
+            elif x_H == x_T:
+                # same direction
+                if y_H > y_T:
+                    y_T += 1
+                else:
+                    y_T -= 1
+            elif y_H == y_T:
+                # same direction
+                if x_H > x_T:
+                    x_T += 1
+                else:
+                    x_T -= 1
+            else:
+                # step diagonally
+                if x_H > x_T:
+                    x_T += 1
+                else:
+                    x_T -= 1
+                if y_H > y_T:
+                    y_T += 1
+                else:
+                    y_T -= 1
+
+            knots[i] = (x_T, y_T)
+        visited.add(knots[-1])
+
+print(len(visited))
